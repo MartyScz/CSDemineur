4de3dd06d1d28b877c950753950d89ab9bf68e1a
MartyScz <sanchezmarty1@yahoo.com>
2026-08-29 17:09:09 +0200

    Ajout de l'affichage des ciffres sur les cases révélées

--- scripts/CaseView.cs
+++ scripts/CaseView.cs
@@ -6,9 +6,13 @@ using Godot;
 
 public partial class CaseView : TextureButton
 {
-    [Export]
     // Ajoute une case de propriété dans l'inspecteur de Godot
+    [Export]
     private Texture2D _revealedTexture;
+
+    // Contient les textures des chiffres de 1 à 8
+    [Export]
+    private Texture2D[] _numberTextures;
 
     // Référence vers le TextureRect utilisé pour afficher le contenu de la case
     private TextureRect _content;
@@ -39,9 +43,22 @@ public partial class CaseView : TextureButton
     // Met à jour l'état visuel de la case selon son état logique
     public void Refresh()
     {
+        // Met à jour l'affichage uniquement si la case est révélée
         if (_gameCase.IsRevealed)
         {
+            // Affiche le don d'une case révélée
             ShowRevealed();
+
+            // Affiche un chiffre uniquement si une ou plusieurs mines sont adjacentes
+            if (_gameCase.AdjacentMines > 0)
+            {
+                // Convertit le nombre de mines adjacentes en index du tableau de textures
+                int textureIndex = _gameCase.AdjacentMines - 1;
+
+                // Affiche la texture du chiffre correspondant
+                _content.Texture = _numberTextures[textureIndex];
+            }
         }
+
     }
 }
